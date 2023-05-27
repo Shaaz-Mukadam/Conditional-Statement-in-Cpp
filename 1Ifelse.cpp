3cd964714de747ccc9c2197afd151a02248185cb
Shaaz Mukadam <84740787+Shaaz-Mukadam@users.noreply.github.com>
2023-05-28 01:54:45 +0530

    Add files via upload
    You may learn basic theory related to Conditional statement and simple code

---
+++ 1Ifelse.cpp
@@ -0,0 +1,30 @@
+//****************** if - else theory ******************
+
+// If statement is used when you have one condiiton and that condition is true the we use if condition
+//Else condition is use when there is more than 1 condition and from both we have to check then if else condition is use
+//if else if when there is multiple conditions
+//if - if else when there is multiple single conditions then we use
+
+//Q. ************************* SAMPLE PROGRAM FOR IF-ELSE COndition****************
+#include<iostream>
+using namespace std;
+int main() {
+    int savings;
+    cin>>savings;
+
+    if(savings>1000) {
+            if(savings>3000){
+                cout<<"RoadTrip with mahvish \n";
+            }
+            else{
+                count<<"Shopping with mahvish \n";
+            }
+        cout<<"Todays date with Mahvish\n";
+    }
+    else{
+        cout<<"Todays no date\n";
+    }
+}
+
+//-------------------------------------------------------------------------------------------------------
+
